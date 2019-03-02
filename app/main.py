c338515b8f8760ca050eddcd3c0a693ac2e6054b
chunthebear <zyc5026@hotmail.com>
2019-03-02 14:56:25 -0800

    updated main

--- app/main.py
+++ app/main.py
@@ -2,25 +2,53 @@
 from flask import Flask, request, jsonify
 import json
 import random
+import bottle 
 
 
 from api import ping_response, start_response, move_response, end_response
 
+@bottle.route('/')
+def index():
+    return '''
+    Battlesnake documentation can be found at
+       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
+    '''
 
-app = Flask(__name__)
+@bottle.route('/static/<path:path>')
+def static(path):
+    """
+    Given a path, return the static file located relative
+    to the static folder.
+    This can be used to return the snake head URL in an API response.
+    """
+    return bottle.static_file(path, root='static/')
 
-@app.route("/start", methods=["GET","HEAD","POST","PUT"])
+@bottle.post('/ping')
+def ping():
+    """
+    A keep-alive endpoint used to prevent cloud application platforms,
+    such as Heroku, from sleeping the application instance.
+    """
+    return ping_response()
+
+
+#app = Flask(__name__)
+
+@bottle.post('/start')
 def start():
-	print(request.data)
-	snake = {
+	data = bottle.request.json
+	print(json.dumps(data))
+	'''snake = {
 		"color": "#ffff00",
 		"name": "InDevelopment"
-	}
-	return jsonify(snake)
+	}'''
+	color = "#00FF00"
+	return start_response(color)
 
-@app.route("/move", methods=["GET","HEAD","POST","PUT"])
+@bottle.post('/move')
 def move():
-	dataStr = request.data
+	dataStr = bottle.request.json
+	print(json.dumps(data))
 	global jsonData
 	jsonData = json.loads(dataStr.decode('utf-8'))
 	#print(jsonData)
@@ -81,7 +109,7 @@ def move():
 	response = {
 		"move": Direction
 	}
-	return jsonify(response)
+	return move_response(direction)
 
 def attack(x, y):
 	target = closestSnake()
@@ -375,6 +403,26 @@ def dontDieThisTurn(jsonData, x, y, Direction):
 
 	return Direction
 
-if __name__ == "__main__":
-	# Don't forget to change the IP address before you try to run it locally
-	app.run(host='192.168.97.167', port=8081, debug=True)
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+
+    """
+    TODO: If your snake AI was stateful,
+        clean up any stateful objects here.
+    """
+    print(json.dumps(data))
+
+    return end_response()
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+
+if __name__ == '__main__':
+    bottle.run(
+        application,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=os.getenv('PORT', '8080'),
+        debug=os.getenv('DEBUG', True)
+    )
